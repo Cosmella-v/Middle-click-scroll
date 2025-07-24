ca8be00dddefa474bc4a795fb3227d64d8bd1b84
Viper <momo2010sofia@gmail.com>
2025-07-24 21:11:01 +0100

    first commit

---
+++ changelog.md
@@ -0,0 +1,2 @@
+# 1.0.0
+- Scrolling works with geode scrollbar and gd scrolls
---
+++ src/main.cpp
@@ -0,0 +1,242 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/TableView.hpp>
+#include <Geode/modify/CCScrollLayerExt.hpp>
+
+#ifdef GEODE_IS_WINDOWS
+#include <windows.h>
+#elif GEODE_IS_MACOS
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+using namespace geode::prelude;
+
+#ifdef GEODE_IS_WINDOWS
+
+enum Direction {
+    NONE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+
+void setCursorForDirection(Direction dir) {
+    HCURSOR cursor = NULL;
+
+    switch (dir) {
+        case UP:
+            cursor = LoadCursor(NULL, MAKEINTRESOURCE(32655)); break;
+        case DOWN:
+		 	cursor = LoadCursor(NULL, MAKEINTRESOURCE(32656)); break;
+        case RIGHT:
+		 	cursor = LoadCursor(NULL, MAKEINTRESOURCE(32657)); break;
+        case LEFT:
+            cursor = LoadCursor(NULL, MAKEINTRESOURCE(32658)); break;
+        case NONE:
+        default:
+            cursor = LoadCursor(NULL, MAKEINTRESOURCE(32654)); break; 
+    }
+
+    if (cursor) {
+        SetCursor(cursor);
+    }
+}
+
+
+void resetCursor() {
+    SetCursor(LoadCursor(NULL, IDC_ARROW));
+}
+#endif
+
+bool isMiddleClickPressed()
+{
+#ifdef GEODE_IS_WINDOWS
+	return (GetAsyncKeyState(VK_MBUTTON) & 0x8000);
+#elif GEODE_IS_MACOS
+	return CGEventSourceButtonState(kCGEventSourceStateHIDSystemState, kCGMouseButtonCenter);
+#else
+	return false;
+#endif
+}
+
+
+float scrollSpeedFactor = 0.1f;
+float maxSpeed = 50.0f;
+
+class $modify(TableViewMouseScroll,TableView)
+{
+	struct Fields
+	{
+		bool isScrolling = false;
+		bool m_movement = false;
+		bool m_Scheduled = false;
+		CCPoint initialClickPos;
+		CCPoint scrollVelocity;
+		bool currentInput = false;
+	};
+	#ifdef GEODE_IS_WINDOWS
+		void OnExit() {
+			resetCursor();
+			return TableView::onExit();
+		};
+	#endif
+	void registerWithTouchDispatcher() {
+		if (m_fields->m_Scheduled == false) {
+			m_fields->m_Scheduled = true;
+			if (m_unused2) {
+       			this->schedule(schedule_selector(TableViewMouseScroll::ForceScrollUpdate));
+			}
+		}
+        return TableView::registerWithTouchDispatcher(); // HOLY SHIT A ALMOST INIT FUNCTION!
+    }
+	bool PositionOverlap(CCPoint Provided) {
+		CCNode* Parent = this->getParent();
+		if (Parent == nullptr) Parent = this;
+		return this->boundingBox().containsPoint(Parent->convertToNodeSpace(Provided));
+	}
+
+	void ForceScrollUpdate(float dt)
+	{
+		bool CurrentStagedInput = isMiddleClickPressed();
+		auto mousePos = getMousePos();
+		if (CurrentStagedInput == m_fields->currentInput) goto Updated;
+		m_fields->currentInput = CurrentStagedInput;
+
+		if (CurrentStagedInput || m_fields->m_movement)
+		{
+			if (!m_fields->isScrolling)
+			{
+				if (!PositionOverlap(mousePos)) return;
+				m_fields->isScrolling = true;
+				m_fields->initialClickPos = mousePos;
+				m_fields->scrollVelocity = CCPointZero;
+			}
+			else
+			{
+				m_fields->m_movement = false;
+				m_fields->isScrolling = false;
+				#ifdef GEODE_IS_WINDOWS
+					resetCursor();
+				#endif
+			}
+		}
+		goto Updated;
+		Updated:
+			if (m_fields->isScrolling)
+			{
+
+				CCPoint offset = ccpSub(m_fields->initialClickPos,mousePos);
+				m_fields->scrollVelocity = ccpMult(offset, scrollSpeedFactor);
+
+				// Clamp
+				m_fields->scrollVelocity.x = std::clamp(m_fields->scrollVelocity.x, -maxSpeed, maxSpeed);
+				m_fields->scrollVelocity.y = std::clamp(m_fields->scrollVelocity.y, -maxSpeed, maxSpeed);
+				m_fields->m_movement = m_fields->scrollVelocity.y != 0 || m_fields->scrollVelocity.x != 0;
+				#ifdef GEODE_IS_WINDOWS
+					// windows only supports the mouse icon thing since i have no idea who didn't care lol, i didn't care enough to make some lol
+					if (m_fields->m_movement) {
+						if (m_fields->scrollVelocity.y > 0) {
+							setCursorForDirection(DOWN);
+						} else if (m_fields->scrollVelocity.y < 0)  {
+							setCursorForDirection(UP);
+						} /*else if ((m_fields->scrollVelocity.x > 0)) {
+							setCursorForDirection(RIGHT);
+						} else {
+							setCursorForDirection(LEFT);
+						};*/
+					} else {
+						setCursorForDirection(NONE);
+					};
+				#endif
+				scrollWheel(m_fields->scrollVelocity.y,m_fields->scrollVelocity.x);
+			};
+			return;
+	}
+};
+
+class $modify(ScrollLayerMouseScroll,CCScrollLayerExt)
+{
+	struct Fields
+	{
+		bool isScrolling = false;
+		bool m_movement = false;
+		bool m_Scheduled = false;
+		CCPoint initialClickPos;
+		CCPoint scrollVelocity;
+		bool currentInput = false;
+	};
+	void registerWithTouchDispatcher() {
+		if (m_fields->m_Scheduled == false) {
+			m_fields->m_Scheduled = true;
+			if (!m_disableMovement) {
+       			this->schedule(schedule_selector(ScrollLayerMouseScroll::ForceScrollUpdate));
+			}
+		}
+        return CCScrollLayerExt::registerWithTouchDispatcher(); // HOLY SHIT A ALMOST INIT FUNCTION!
+    }
+	bool PositionOverlap(CCPoint Provided) {
+		CCNode* Parent = this->getParent();
+		if (Parent == nullptr) Parent = this;
+		return this->boundingBox().containsPoint(Parent->convertToNodeSpace(Provided));
+	}
+
+	void ForceScrollUpdate(float dt)
+	{
+		bool CurrentStagedInput = isMiddleClickPressed();
+		auto mousePos = getMousePos();
+		if (CurrentStagedInput == m_fields->currentInput) goto Updated;
+		m_fields->currentInput = CurrentStagedInput;
+
+		if (CurrentStagedInput || m_fields->m_movement)
+		{
+			if (!m_fields->isScrolling)
+			{
+				if (!PositionOverlap(mousePos)) return;
+				m_fields->isScrolling = true;
+				m_fields->initialClickPos = mousePos;
+				m_fields->scrollVelocity = CCPointZero;
+			}
+			else
+			{
+				m_fields->m_movement = false;
+				m_fields->isScrolling = false;
+				#ifdef GEODE_IS_WINDOWS
+					resetCursor();
+				#endif
+			}
+		}
+		goto Updated;
+		Updated:
+			if (m_fields->isScrolling)
+			{
+
+				CCPoint offset = ccpSub(m_fields->initialClickPos,mousePos);
+				m_fields->scrollVelocity = ccpMult(offset, scrollSpeedFactor);
+
+				// Clamp
+				m_fields->scrollVelocity.x = std::clamp(m_fields->scrollVelocity.x, -maxSpeed, maxSpeed);
+				m_fields->scrollVelocity.y = std::clamp(m_fields->scrollVelocity.y, -maxSpeed, maxSpeed);
+				m_fields->m_movement = m_fields->scrollVelocity.y != 0 || m_fields->scrollVelocity.x != 0;
+				#ifdef GEODE_IS_WINDOWS
+					// windows only supports the mouse icon thing since i have no idea who didn't care lol, i didn't care enough to make some lol
+					if (m_fields->m_movement) {
+						if (m_disableVertical) {
+							setCursorForDirection(NONE);
+						} else {
+							if (m_fields->scrollVelocity.y > 0) {
+								setCursorForDirection(DOWN);
+							} else if ( m_fields->scrollVelocity.y < 0)  {
+								setCursorForDirection(UP);
+							} 
+						}
+					} else {
+						setCursorForDirection(NONE);
+					};
+				#endif
+				scrollLayer(m_fields->scrollVelocity.y);
+			};
+			return;
+	}
+};
+
