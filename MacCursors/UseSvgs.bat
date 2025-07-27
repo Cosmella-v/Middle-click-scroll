9b014aceb31c06c939cc39547215dd05c2717d5e
Viper <momo2010sofia@gmail.com>
2025-07-28 00:23:32 +0100

    fix files not being used right

--- MacCursors/UseSvgs.bat
+++ MacCursors/UseSvgs.bat
@@ -9,9 +9,9 @@ for %%F in (*.svg) do (
     echo Processing: %%F
     %INKSCAPE% "%%F" --export-area-drawing --export-width=%SIZE1% --export-filename="%%~nF.png"
 	echo %SIZE1% done!
-    %INKSCAPE% "%%F" --export-area-drawing --export-width=%SIZE2% --export-filename="%%~nF@%SIZE2%.png"
+    %INKSCAPE% "%%F" --export-area-drawing --export-width=%SIZE2% --export-filename="%%~nF_%SIZE2%.png"
 	echo %SIZE2% done!
-    %INKSCAPE% "%%F" --export-area-drawing --export-width=%SIZE3% --export-filename="%%~nF@%SIZE3%.png"
+    %INKSCAPE% "%%F" --export-area-drawing --export-width=%SIZE3% --export-filename="%%~nF_%SIZE3%.png"
 	echo %SIZE3% done!
 )
 
